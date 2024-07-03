cc325276a135561520b1d50ac3848a462ed52b54
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-07-03 19:36:18 +0530

    minimal_build

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { // "!" is never type, since function should never return anything
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(_test: &[&dyn Fn()]) {
+    loop {}
+}
