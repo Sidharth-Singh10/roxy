9c6e8d36ea539d00070f30e21b62d752e00ef603
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-07-04 23:06:32 +0530

    vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,14 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(test_runner)]
 use core::panic::PanicInfo;
+mod vga_buffer;
 
-
-static HELLO: &[u8] = b"Hello World!";  //byte string 
+// static HELLO: &[u8] = b"Hello World!";  //byte string 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;// VGA text mode ka address
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            //Writes the character byte at position i * 2. 
-            //Each character cell in VGA text mode consists of two bytes: one for the character and one for the attribute (color).
-            *vga_buffer.offset(i as isize * 2) = byte;
-            //Writes the color attribute byte next to the character byte
-            //0xb represents a light cyan foreground color on a black background.
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    
 
     loop {}
 }
