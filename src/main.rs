724c9c6fa60cbe9a69150bb2cb4d5ce289df1223
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-07-06 18:55:32 +0530

    testing_phase2

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,32 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
+#![test_runner(roxy::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
-mod serial;
-mod vga_buffer;
-// static HELLO: &[u8] = b"Hello World!";  //byte string
+use roxy::println;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
     #[cfg(test)]
     test_main();
 
     loop {}
 }
-#[panic_handler]
+
+/// This function is called on panic.
 #[cfg(not(test))]
-fn panic(_info: &PanicInfo) -> ! {
-    // "!" is never type, since function should never return anything
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)] //The enum is marked as #[repr(u32)] to represent each variant by a u32 integeR
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+    roxy::test_panic_handler(info)
 }
